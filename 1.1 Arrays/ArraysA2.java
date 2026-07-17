447e1ac550ca720cbec29cb3bfceb959325ad0cc
Rishavraj Singh <rishavpce@gmail.com>
2026-07-17 18:56:32 +0530

    array question

---
+++ 1.1 Arrays/ArraysA2.java
@@ -0,0 +1,29 @@
+//2 --> Sum of Arrays
+
+import java.util.Scanner;
+
+public class ArraysA2 {
+
+    public static int SumofArray(int arr[]) {
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(SumofArray(arr));
+
+    }
+    
+}
