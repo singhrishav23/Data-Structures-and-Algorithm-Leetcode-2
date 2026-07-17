bc718ba7a641575b7e815062ee3c48c94020a6c8
Rishavraj Singh <rishavpce@gmail.com>
2026-07-17 18:57:19 +0530

    array question

---
+++ 1.1 Arrays/ArraysA3.java
@@ -0,0 +1,32 @@
+//3 --> Max of all elements in a array
+
+import java.util.Scanner;
+
+public class ArraysA3 {
+    
+    public static int maxElement(int arr[]) {
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(maxElement(arr));
+    }
+    
+}
